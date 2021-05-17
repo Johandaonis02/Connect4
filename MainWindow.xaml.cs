12286c180fab692e179307eb79e912a28a4d8d74
Johandaonis02 <Johan.Daun@elev.ga.ntig.se>
2021-05-17 19:35:01 +0200

    Start of TestIfWon method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
 
 namespace Connect4
 {
@@ -21,25 +22,109 @@ namespace Connect4
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
         MediaPlayer player = new MediaPlayer();
-        int[,] cells = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+        ImageBrush backgroundimage = new ImageBrush();
+
+        int time;
+        int[,] cells = {{0, 0, 0, 0, 0, 0, 0}, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0 }};
         public MainWindow()
         {
             InitializeComponent();
+            
+            gameTimer.Tick += GameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(17);
+
+            /*
+            backgroundimage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/pictures/cell.png"));
+            Connect4.Background = backgroundimage;
+            */
         }
         //test
         private void GameEngine(object sender, EventArgs e)
         {
-            drawBoard();
+            Time1.Content = "Score";
+            //Time1.Content = "Score: " + time;
+            //time++;
+            //drawBoard();
             
         }
 
-        public void drawBoard(){
+        public void removeBoard(){
             foreach (var x in Connect4.Children.OfType<Rectangle>())
             {
-                Connect4.Children.Remove(x);     
+                Connect4.Children.Remove(x);
+            }
+        }
+
+        public bool TestIfWon()
+        {
+            int player = 1; //Detta ska inte alltid vara 1 men jag fixar det sen.
+            bool FourInARow = true;
+
+            //row
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    FourInARow = true;
+                    
+                    for (int xOffset = 0; xOffset < 4; xOffset++)
+                    {
+                        if(cells[x + xOffset, y] != player)
+                        {
+                            FourInARow = false;
+                        } 
+                    }
+                    
+                    if (FourInARow)
+                    {
+                        return (true);
+                    }
+                    
+                }
             }
 
+            //column
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    FourInARow = true;
+
+                    for (int yOffset = 0; yOffset < 4; yOffset++)
+                    {
+                        if (cells[x + yOffset, y] != player)
+                        {
+                            FourInARow = false;
+                        }
+                    }
+
+                    if (FourInARow)
+                    {
+                        return (true);
+                    }
+                }
+            }
+        }
+        public void drawBoard(){
+
+            ImageBrush CellImage = new ImageBrush();
+            CellImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/pictures/cell.png"));
+
+            Rectangle newCell = new Rectangle
+            {
+                Tag = "cell",
+                Height = 50,
+                Width = 50,
+                Fill = CellImage
+            };
+
+            Canvas.SetLeft(newCell, 600);
+            Canvas.SetTop(newCell, 600);
+
+            Connect4.Children.Add(newCell);
         }
 
         private void PlacePiece(object sender, MouseButtonEventArgs e)
